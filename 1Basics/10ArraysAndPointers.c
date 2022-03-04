a52fabaf896d9157199071c56d028113e55e020d
melky-melk <chiara.justine.tan@gmail.com>
2022-03-04 14:05:54 +1100

    adding initializing variables notes
    from lecture 2

--- 1Basics/10ArraysAndPointers.c
+++ 1Basics/10ArraysAndPointers.c
@@ -11,12 +11,12 @@ int main(){
     */
 	
 	int values[100];
-    int *valuesPtr;
+    int *valuesPtr; //have to use malloc lol
 
     // when you call the variable name for the array, it just gives the pointer to the first element in the values array 
     // so when making a pointer go to an array you dont need to use the & to get the memory address 
     valuesPtr = values; 
-    //it would be the same thing as
+    //it would be the same thing as getting the address of the first element in the array
     valuesPtr = &values[0]
 
     i = 0;
@@ -28,6 +28,13 @@ int main(){
     //increments the pointer to 3 ahead, the 4th element. and dereferences. so it grabs the value at valuesPtr[3]
     *(valuesPtr + 3);
 
-    //to set values inside you can increment by which index you want and dereference it 
+    //to set values inside you can increment by which index you want and dereference it valuesPt
     *(valuesPtr + 10) = 27;
+
+	// FIXME note this will not work, the following comments are to explain why this would not work
+	char x[10]; // the specific area of memory is reserved to be size 10. char x[10] is constant information that has to be persistant within the entirety of the program
+	x = "hello" // by assigning x as "hello" its essentially trying to reassign the value of x, but x should already be constant 
+	// this will most likely result in a seg fault 
+
+	// if you were to print a string array, it would keep printing everything until it hits a null byte
 }
